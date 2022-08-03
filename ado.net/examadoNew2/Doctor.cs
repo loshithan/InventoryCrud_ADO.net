e6870709a53c603583e5cad77fbdda3c8180e960
loshithan <loshithanparamaguru77@gmail.com>
2022-08-03 19:40:12 +0530

    first commit

---
+++ ado.net/examadoNew2/Doctor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examadoNew2
+{
+    class Doctor
+    {
+        private int _id;
+        private string _name;
+        private string _qualification;
+        private int _experience;
+        private double _fees;
+
+        public Doctor()
+        {
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Qualification { get => _qualification; set => _qualification = value; }
+        public double Fees { get => _fees; set => _fees = value; }
+        public int Experience { get => _experience; set => _experience = value; }
+
+        public Doctor(int _id, string _name, string _qualification, int _experience, double _fees)
+        {
+            this.Id = _id;
+            this.Name = _name;
+            this.Qualification = _qualification;
+            this.Experience = _experience;
+            this.Fees = _fees;
+        }
+
+        public Doctor(string _name, string _qualification, int _experience, double _fees)
+        {
+            this.Name = _name;
+            this.Qualification = _qualification;
+            this.Experience = _experience;
+            this.Fees = _fees;
+        }
+    }
+}
---
+++ ado.net/examadoNew2/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace examadoNew2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            DoctorBO bo = new DoctorBO();
+            Console.WriteLine("Menu");
+            Console.WriteLine("1.Insert doctor");
+            Console.WriteLine("2.Update fees");
+            Console.WriteLine("3.Delete doctor");
+            Console.WriteLine("4.Exit");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the name");
+                    //Fill your code here
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter the qualification");
+                    //Fill your code here
+                    string qualification = Console.ReadLine();
+                    Console.WriteLine("Enter the experience");
+                    //Fill your code here
+                    int exp = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the fees");
+                    //Fill your code here
+                    double fees = Convert.ToDouble(Console.ReadLine());
+                    
+                    if (bo.InsertDoctor(new Doctor(name,qualification,exp,fees)))
+                    {
+                        Console.WriteLine("Doctor inserted successfully");
+                        //Fill your code here
+                        bo.DisplayDoctors();
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the doctor id");
+                    //Fill your code here
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the fees");
+                    //Fill your code here
+                    double upFees = Convert.ToDouble(Console.ReadLine());
+
+                    if (bo.UpdateDoctor(id,upFees))
+                    {
+                        Console.WriteLine("Doctor updated successfully");
+                        //Fill your code here
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid doctor id");
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Enter the doctor id");
+                    //Fill your code here
+                    int delId = Convert.ToInt32(Console.ReadLine());
+                    if (bo.DeleteDoctor(delId))
+                    {
+                        Console.WriteLine("Doctor deleted successfully");
+                        //Fill your code here
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid doctor id");
+                    }
+                    break;
+                case 4:
+                    break;
+
+                default:
+                    //Fill your code here
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+    }
+}
